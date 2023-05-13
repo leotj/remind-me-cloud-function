0a9187ea8a50a938a65dbcee811a2452e96f0a41
Leo Tiofan Justicia <general@leotj.com>
2023-05-13 20:09:35 +0700

    Add send reminder function

--- functions/Send Reminder/src/index.js
+++ functions/Send Reminder/src/index.js
@@ -1,4 +1,6 @@
 const sdk = require("node-appwrite");
+const https = require("https");
+const querystring = require("querystring");
 
 /*
   'req' variable has:
@@ -27,20 +29,73 @@ module.exports = async function (req, res) {
   const teams = new sdk.Teams(client);
   const users = new sdk.Users(client);
 
+  const databaseID = "642022e1e1bf13aa04b9";
+  const collectionID = {
+    reminders: "642023bce66fe86cc9c8",
+  };
+
   if (
-    !req.variables['APPWRITE_FUNCTION_ENDPOINT'] ||
-    !req.variables['APPWRITE_FUNCTION_API_KEY']
+    !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+    !req.variables["APPWRITE_FUNCTION_API_KEY"]
   ) {
-    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
+    console.log(
+      "Environment variables are not set. Function cannot use Appwrite SDK."
+    );
+  } else if (
+    !req.variables["ZULIP_BOT_EMAIL_ADDRESS"] ||
+    !req.variables["ZULIP_BOT_API_KEY"] ||
+    !req.variables["ZULIP_PERSONAL_EMAIL"]
+  ) {
+    console.log(
+      "Environment variables are not set. Function cannot call Zulip Chat REST API."
+    );
   } else {
     client
-      .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
-      .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
-      .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
+      .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+      .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+      .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"])
       .setSelfSigned(true);
+
+    try {
+      const { total, documents } = await database.listDocuments(
+        databaseID,
+        collectionID.reminders
+      );
+
+      const randomizedIndex = Math.floor(Math.random() * (total + 1));
+
+      const options = {
+        host: "leotj.zulipchat.com",
+        port: 443,
+        path: "/api/v1/messages",
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${req.variables["ZULIP_BOT_EMAIL_ADDRESS"]}:${req.variables["ZULIP_BOT_API_KEY"]}`
+          ).toString("base64")}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+
+      const data = querystring.stringify({
+        type: "direct",
+        to: req.variables["ZULIP_PERSONAL_EMAIL"],
+        content: documents[randomizedIndex].message,
+      });
+
+      const request = https.request(options, (response) => {});
+      request.on("error", (error) => {
+        console.log("Sending Zulip Chat direct message failed.");
+        console.log(error);
+      });
+      request.write(data);
+      request.end();
+    } catch (e) {
+      console.log("error", e);
+    }
   }
 
   res.json({
-    areDevelopersAwesome: true,
+    success: true,
   });
 };
